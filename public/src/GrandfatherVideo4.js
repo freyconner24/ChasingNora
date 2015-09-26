0f44d4080979437f740fb5d7fec35c5c34389752
Conner Frey <frey.conner24@gmail.com>
2015-09-25 21:34:00 -0700

    Added video 4 to index.html

---
+++ public/src/GrandfatherVideo4.js
@@ -0,0 +1,12 @@
+var GrandfatherVideo4 = React.createClass({
+  render: function() {
+    return (
+      <div className='block darkBlueBack white textCenter bigBelle'>
+          <div className='marvinsRoom'>"the dead, they waltz" (video 4)</div>
+          <div className="embed-responsive embed-responsive-16by9">
+              <iframe className='embed-responsive-item myPadding' src="https://www.youtube.com/embed/PR2KcT9Ilo0" frameBorder="0" allowFullScreen></iframe>
+          </div>
+      </div>
+    );
+  }
+});

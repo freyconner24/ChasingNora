f1a959aae4366132fa34bbe3dc7a0fd2ccc0b51d
Conner <frey.conner24@gmail.com>
2016-02-22 17:30:00 -0800

    Update Header.js

--- public/src/Header.js
+++ public/src/Header.js
@@ -2,7 +2,7 @@ var Header = React.createClass({
       render: function() {
         return (
             <div className='myHeader centerBlock textCenter white'>
-                <div className='headerItem inlineBlock'><a href="https://soundcloud.com/polarmusicstudios">soundcloud</a></div>
+                <div className='headerItem inlineBlock'><a href="https://soundcloud.com/chasingnora">soundcloud</a></div>
                 <div className='headerItem inlineBlock'><a href="https://chasingnora.bandcamp.com/music">bandcamp</a></div>
                 <div className='headerItem inlineBlock'><a href="https://www.youtube.com/channel/UCu0T4YHejamtgCc1aQjqdSg">youtube</a></div>
                 <div className='headerItem inlineBlock'><a href="https://instagram.com/chasingnora/">instagram</a></div>
